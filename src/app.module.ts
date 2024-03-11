97f1b3cd948a0ac178495c6b8a3ad8d694cbb1ce
shimjaedoo <woen1637@naver.com>
2024-03-11 18:48:39 +0900

    chore : build

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,23 @@
-import { Module } from "@nestjs/common";
+import { Module } from '@nestjs/common';
+import { PrismaModule } from './_common/prisma/prisma.module';
+import { MemberModule } from './member/member.module';
+import { BcryptModule } from './_common/bcrypt/bcrypt.module';
+import { JwtModule } from './_common/jwt/jwt.module';
+import { AuthModule } from './auth/auth.module';
+import { SnsModule } from './sns/sns.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
+  imports: [
+    PrismaModule,
+    MemberModule,
+    BcryptModule,
+    JwtModule,
+    AuthModule,
+    SnsModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+  ],
 })
 export class AppModule {}
